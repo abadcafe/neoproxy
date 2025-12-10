71cf7e3bfc51b7b1d593c1d66c5f5994fc5c2cad
abadcafe <fwlei@live.com>
2025-12-11 02:52:47 +0800

    added http3 upstream connecting.

--- src/plugins/service_http3_upstream.rs
+++ src/plugins/service_http3_upstream.rs
@@ -1,13 +1,97 @@
 use std::collections::HashMap;
+use std::net as std_net;
+use std::path::PathBuf;
 use std::pin::Pin;
+use std::sync::Arc;
 use std::task::{Context, Poll};
-use tokio::net;
 
 use anyhow::{Result, anyhow};
+use h3_quinn::quinn;
+use rustls::pki_types::CertificateDer;
+use rustls_native_certs::CertificateResult;
+use tokio::net;
+use tracing::{error, info, log};
 
 use crate::plugin;
 
-struct Http3UpstreamService {}
+static ALPN: &[u8] = b"h3";
+
+struct Upstream {
+  addresses: Vec<std_net::SocketAddr>,
+  ca_path: PathBuf,
+}
+
+impl Upstream {
+  fn new(
+    ca_path: PathBuf,
+    addresses: Vec<std_net::SocketAddr>,
+  ) -> Self {
+    Self { addresses, ca_path }
+  }
+
+  async fn get_connection(&self) -> Result<h3_quinn::Connection> {
+    let mut roots = rustls::RootCertStore::empty();
+    let CertificateResult { certs, errors, .. } =
+      rustls_native_certs::load_native_certs();
+    for cert in certs {
+      if let Err(e) = roots.add(cert) {
+        error!("failed to parse trust anchor: {}", e);
+      }
+    }
+    for e in errors {
+      error!("couldn't load default trust roots: {}", e);
+    }
+
+    // load certificate of CA who issues the server certificate
+    // NOTE that this should be used for dev only
+    if let Err(e) = roots
+      .add(CertificateDer::from(std::fs::read(self.ca_path.as_path())?))
+    {
+      error!("failed to parse trust anchor: {}", e);
+    }
+
+    let mut tls_config = rustls::ClientConfig::builder()
+      .with_root_certificates(roots)
+      .with_no_client_auth();
+
+    tls_config.enable_early_data = true;
+    tls_config.alpn_protocols = vec![ALPN.into()];
+
+    // Write all Keys to a file if SSLKEYLOGFILE is set
+    // WARNING, we enable this for the example, you should think
+    // carefully about enabling in your own code
+    tls_config.key_log = Arc::new(rustls::KeyLogFile::new());
+
+    let mut client_endpoint =
+      h3_quinn::quinn::Endpoint::client("[::]:0".parse().unwrap())?;
+
+    let client_config = quinn::ClientConfig::new(Arc::new(
+      quinn::crypto::rustls::QuicClientConfig::try_from(tls_config)?,
+    ));
+    client_endpoint.set_default_client_config(client_config);
+
+    let conn = client_endpoint
+      .connect(
+        self.addresses[0],
+        self.addresses[0].to_string().as_str(),
+      )?
+      .await?;
+
+    info!("QUIC connection established");
+
+    // create h3 client
+
+    // h3 is designed to work with different QUIC implementations via
+    // a generic interface, that is, the [`quic::Connection`] trait.
+    // h3_quinn implements the trait w/ quinn to make it work with h3.
+    let quinn_conn = h3_quinn::Connection::new(conn);
+    Ok(quinn_conn)
+  }
+}
+
+struct Http3UpstreamService {
+  upstream: Upstream,
+}
 
 fn build_empty_response(
   status_code: http::status::StatusCode,
@@ -66,11 +150,10 @@ impl tower::Service<plugin::Request> for Http3UpstreamService {
         (dest.host(), port)
       };
 
-      let addr = net::lookup_host((dest_host, dest_port))
+      let dest_addr = net::lookup_host((dest_host, dest_port))
         .await?
         .next()
         .ok_or(anyhow!("dns found no addresses"))?;
-
 
       Err(anyhow!("todo!"))
     })
